a65f97df10c7e68c55e89af830bd38449157128c
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2026-02-23 15:08:55 +0900

    4.1.0

--- type.ts
+++ type.ts
@@ -1,43 +1,23 @@
-function superPrint<V>(a: V[]) {
-  return a[0];
+abstract class User {
+  constructor(
+    protected firstName: string,
+    protected lastName: string,
+    public nickName: string,
+  ) {}
+  abstract getNickName(): void;
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
-const a = superPrint([1, 2, 3, 4]);
-const b = superPrint([true, false, true]);
-const c = superPrint(["a", "b", "c"]);
-const d = superPrint([1, 2, true, false]);
-
-console.log(a, b, c, d);
-
-type Player<E> = {
-  name: string;
-  extraInfo: E;
-};
-type SepiExtra = {
-  favFood: string;
-};
-type SepiPlayer = Player<SepiExtra>;
-
-const sepi: SepiPlayer = {
-  name: "sepi",
-  extraInfo: {
-    favFood: "hamburger",
-  },
-};
-
-const elliot: Player<null> = {
-  name: "elliot",
-  extraInfo: null,
-};
-
-type A = Array<number>;
-
-let a: A = [1, 2, 3, 4];
-
-function printAllNumbers(arr: Array<number>) {
-  arr.forEach((item) => {
-    console.log(item);
-  });
+class Player extends User {
+  getNickName() {
+    console.log(this.nickName);
+  }
 }
 
-printAllNumbers(a);
+const sepi = new Player("sam", "sepi0l", "sepi");
+
+sepi.getFullName();
+sepi.nickName;
