88e1c14638a0aff90da809e632eef74bc56aa872
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2026-02-23 16:45:10 +0900

    4.5.0

--- type.ts
+++ type.ts
@@ -1,10 +1,29 @@
-type PlayerA = {
-  firstName: string;
-};
-interface PlayerB {
-  firstName: string;
+interface SStorage<T> {
+  [key: string]: T;
 }
 
-class User implements PlayerB {
-  constructor(public firstName: string) {}
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
 }
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("key");
+stringsStorage.set("key", "value");
+
+const booleanStorage = new LocalStorage<boolean>();
+
+booleanStorage.get("xxx");
+booleanStorage.set("hello", true);
