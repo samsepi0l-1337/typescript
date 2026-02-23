4c047d216e230c60f5f5980c643021bf60d44ccc
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2026-02-23 15:28:25 +0900

    4.2.5

--- type.ts
+++ type.ts
@@ -1,20 +1,9 @@
-type Team = "red" | "blue" | "yellow";
-type Health = 1 | 5 | 10;
-
-type Player = {
-  nickname: string;
-  team: Team;
-  health: Health;
-};
-
-interface Person {
-  nickname: string;
-  team: Team;
-  health: Health;
+interface User {
+  name: string;
 }
 
+interface Player extends User {}
+
 const sepi: Player = {
-  nickname: "sepi",
-  team: "red",
-  health: 10,
+  name: "sepi",
 };
